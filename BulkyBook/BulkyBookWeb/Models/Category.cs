45676482723d3df7a35a9bc6b618ab64a84414da
S Behera <saradabro@gmail.com>
2022-05-04 19:15:16 +0530

    Completed codefirst CRUD app

--- BulkyBook/BulkyBookWeb/Models/Category.cs
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -9,6 +9,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name ="Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 and 100 only!!!")]
         public int DsiplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
