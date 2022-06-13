e41090906b7cb2628c73980e470c197c854d99fa
S Behera <saradabro@gmail.com>
2022-06-13 17:59:53 +0530

    Completed section 9 "Shopping Cart"

--- BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -18,5 +18,6 @@ namespace BulkyBook.DataAccess
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
     }
 }
--- BulkyBook/BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/Repository.cs
@@ -25,9 +25,15 @@ namespace BulkyBook.DataAccess.Repository
         }
 
         //includeProp - "Category,CoverType"
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter=null, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
             if (includeProperties != null)
             {
                 foreach (var inludeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
